aa426b5da83d8f05f116045d5270d3e24bb8df77
JGabZXC <jlopezgab25@gmail.com>
2026-01-13 12:54:25 +0800

    Update Docker configurations to use environment variables for PostgreSQL credentials and ensure .env file is loaded in settings

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,10 +30,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-kp1q-i^=l!f-=garl6!6w
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,backend").split(",")
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000,http://localhost,http://127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000,http://localhost,http://127.0.0.1").split(",")
+
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000,http://localhost,http://127.0.0.1").split(",")
 
 
 # Application definition
